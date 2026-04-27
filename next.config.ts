6ebcd2c0d3da9875e3cb162cf2e5887bfe529ba0
Arjun <arjukannu@gmail.com>
2026-04-27 16:12:38 +0530

    updated changes 4

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'standalone',
+  output: "standalone",
+  transpilePackages: [
+    "@mui/material",
+    "@mui/system",
+    "@mui/utils",
+    "@mui/icons-material",
+    "@mui/x-data-grid",
+    "@mui/x-date-pickers",
+  ],
+  experimental: {
+    optimizePackageImports: [],
+    webpackBuildWorker: false,
+  },
 };
 
 export default nextConfig;
